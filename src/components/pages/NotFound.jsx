727bf74c644f42c4d076e67931e24f3a4c6ebbc8
jihadcodes <jihad.dev.pro@gmail.com>
2026-03-03 15:49:52 +0600

    404 done

--- src/components/pages/NotFound.jsx
+++ src/components/pages/NotFound.jsx
@@ -6,13 +6,13 @@ const NotFound = () => {
     <>
       <div className="container">
         <div className="min-h-screen flex flex-col items-center justify-center text-center">
-            {/* <h1 className="mb-6 text-[110px] text-[#000000] tracking-[3%] leading-[115px] font-inter font-medium ">
+            <h1 className="mb-6 text-[110px] text-[#000000] tracking-[3%] leading-[115px] font-inter font-medium ">
                 404 Not Found
             </h1>
             <p className="para-text mb-20 "> Your visited page not found. You may go home page.</p>
             <Link to="/">
             <button className="button">Back to home page</button>
-            </Link> */}
+            </Link>
       </div>
       </div>
     </>
