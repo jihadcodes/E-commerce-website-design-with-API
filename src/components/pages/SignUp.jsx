6d6c6b6f46728297af3f0da05eb1c67528fce688
jihadcodes <jihad.dev.pro@gmail.com>
2026-03-03 17:53:51 +0600

    signup page design complete

--- src/components/pages/SignUp.jsx
+++ src/components/pages/SignUp.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import login_img from "../../assets/images/login.png";
+import google from '../../assets/images/google.png'
+import { Link } from 'react-router';
 const SignUp = () => {
   return (
     <>
@@ -12,17 +14,27 @@ const SignUp = () => {
                 <form action="">
     
                 <h3 className="text-[#000000] tracking-[4%] leading-7.5 text-4xl font-inter font-medium mb-6 ">
-                  Log in to Exclusive
+                  Create an account
                 </h3>
                 <p className="para-text mb-12">Enter your details below</p>
-                <input type="text" className="form-input "  placeholder="Email or Phone Number" />
+                <input type="text" className="form-input "  placeholder="Name" />
+                <input type="text" className="form-input"  placeholder="Email or Phone Number" />
                 <input type="password" className="form-input"  placeholder="Password" />
-                
-                    <button className="button">Log In</button>
-                    <a  href="" className="text-[16px] text-[#DB4444] leading-6 font-poppins font-normal ">Forget Password?</a>
+                <div className="flex items-center flex-col  mb-8">
+                    <button className="button px-[122px]! mb-4">Create Account</button>
+                    <div className='py-4 w-full px-[86px] flex items-center gap-4 border mb-8 border-[#00000066] text-center rounded-sm'>
+                        <span >
+                            <img src={google} alt="" />
+                        </span>
+                        <a href=""> Sign up with Google</a>
+                      </div>
+                      <a  href="" className="text-[16px] text-[#DB4444] text-center leading-6 font-poppins font-normal "> Already have account?
+                        <Link className='ml-4 pb-1 border-b border-[#000000] font-medium' to="/login">Log in</Link>
+                         </a>
+                    
+                </div>
                 
                 </form>
-                
               </div>
             </div>
           </div>
