f736fa052d718acbba37fde4877d5395de38ce6b
jihadcodes <jihad.dev.pro@gmail.com>
2026-04-07 21:58:37 +0600

    api call with axios in explore section

--- src/components/Explore/Explore.jsx
+++ src/components/Explore/Explore.jsx
@@ -61,9 +61,7 @@ const Explore = () => {
                 </div>
                 <div className='text-center'>
 
-                    {visibleCount < products.length && (
-                     <button onClick={() => setVisibleCount(visibleCount + 8)} className='font-popins text-[#FAFAFA] text-[16px] leading-6 font-medium bg-[#DB4444] select-none cursor-pointer  py-4 px-12 rounded-sm'>Load More</button>
-                    )}
+                   
                     
                 </div>
             </div>
